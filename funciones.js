5e4b464ffe9ebf0805803e4af627c813f15f0f63
JoseFM20 <joseasaelf@gmail.com>
2023-01-11 09:37:16 -0800

    Ejercicio Dias semanana

---
+++ funciones.js
@@ -0,0 +1,18 @@
+
+        const edad = prompt('Captura tu edad');
+
+        const Checkedad = (edad)=> {
+
+            if (edad >= 100 || edad <= 0) {
+             console.log('Proporciona una edad real  ');
+         }
+         else if (edad < 18 && edad >= 1) {
+             console.log('Eres menor de edad ');
+         }
+         else if
+             (edad >= 18) {
+             console.log('Eres mayor de edad  ');
+
+        }
+        }
+        Checkedad(edad);
---
+++ variables.js
@@ -0,0 +1,36 @@
+var nombre = "Hector";
+        var genero = "Masculino";
+       var nombree= prompt('Dame tu nombre completo');
+
+         var edad = 19;
+        var numalumnos = 15;
+        var estatura = 1.95;
+        var peso = 55.5;
+        var becado = true; 
+        var saludo;
+
+         const saludo = (nombre) => {
+             return nombre;
+ 
+             console.log('Holaa ' + nombre);
+         }
+ 
+         const nombre = saludo("Hector");
+         console.log(nombre);
+   
+         const saludo = (nombre) => {
+            return nombre;
+        
+            console.log('Holaa ' + nombre);
+        }
+                
+                
+                
+        
+        
+               console.log("Hola mi nombre es " + nombre);
+        
+                console.log(`Hola mi nombre es ${nombre} Mi edad es de ${edad} mi estatura es de ${peso} y mi estarura es de ${estatura}`);
+         
+         
+         console.log("Hola desde Java script") 
