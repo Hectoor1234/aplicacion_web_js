170bd93e7dc34533732db1a1063b2adffdeb27b9
JoseFM20 <joseasaelf@gmail.com>
2023-01-13 12:50:55 -0800

    ejercicio de objetos y arreglos

--- diassemana.js
+++ diassemana.js
@@ -2,30 +2,31 @@ const num = prompt('Ingresa un numero');
 
 const Checkdia = (num)=> {
 
-    if (num =1 ) {
+    if (num ==1 ) {
         console.log ('Es lunes ');
     }
-    else if (num = 2) {
+    else if (num ==2) {
         console.log('Es martes')
     }
-    else if (num = 3) {
+    else if (num ==3) {
         console.log('Es Miercoles')
     }
-    else if (num = 4) {
+    else if (num ==4) {
         console.log('Es Jueves')
     }
-    else if (num = 5) {
+    else if (num ==5) {
         console.log('Es Viernes')
     }
-    else if (num = 6) {
+    else if (num ==6) {
         console.log('Es Sabado')
     }
-    else if (num = 7) {
+    else if (num ==7) {
         console.log('Es Domingo')
     }
     else {
         console.log('Ingresa un numero valido ')
     }
 
-    Checkdia(num);
+   
 }
+Checkdia(num);
---
+++ objetos.js
@@ -0,0 +1,18 @@
+const persona = {
+    nombre: 'Hector',
+    edad: 19,
+    genero: 'Masculino',
+    estatura: 1.99, 
+    peso: 20, 
+    email: 'hectornmc@gmail.com'
+}
+const nombre = persona.nombre;
+const edad= persona.edad;
+const genero = persona.genero;
+const estatura= persona.estatura;
+const peso = persona.peso;
+const email = persona.email;
+
+console.log(` mi nombre es ${nombre} 
+tengo ${edad} años, mi genero es ${genero}, mi estatura es ${estatura},mi peso es ${peso} 
+y mi correo es ${email}`);
