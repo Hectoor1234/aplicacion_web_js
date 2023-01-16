07e30cd1fa840796980c5d088420063c8e13a5c0
Hectoor1234 <hectornmc@gmail.com>
2023-01-16 08:03:58 -0700

    Ejercicio consumir api

---
+++ ForEach.js
@@ -0,0 +1,20 @@
+const yo = [
+    {
+        nombre: 'Cesar',
+        edad: 25,
+        estatura: 1.50,
+    },
+    {
+        nombre: 'Hector',
+        edad: 19,
+        estatura: 1.90,
+    },
+    {
+        nombre: 'Aaron',
+        edad: 19,
+        estatura: 1.42,
+    },
+];
+yo.forEach((yo) => {
+    console.log(yo);
+});
