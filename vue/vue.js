74d9f1775ec21902135f25fa0e12e921784284e7
JoseFM20 <joseasaelf@gmail.com>
2023-01-17 09:18:49 -0800

    Ejercicio vue

---
+++ vue/vue.js
@@ -0,0 +1,26 @@
+Vue.createApp({
+    data() {
+        return {
+        contador:0
+
+    }
+},
+    methods: {
+        incrementar(){
+            this.contador++;
+
+        },
+
+
+       
+        decrementar(){
+            this.contador--;
+        },
+    
+
+    },
+
+
+    
+   
+}).mount("#app");
