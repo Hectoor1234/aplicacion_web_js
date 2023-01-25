7de5a555b7e810ded610ce6649622200bead0e60
JoseFM20 <joseasaelf@gmail.com>
2023-01-25 09:41:01 -0800

    carrito

---
+++ carrito/carrito.js
@@ -0,0 +1,26 @@
+Vue.createApp({
+    data(){
+        return{
+            
+                productos:[],
+                nombre:'',
+                precio:'',
+                cantidad:''
+                  
+                
+            
+        }
+    },  
+    methods: {
+        agregar(){
+
+            const producto = {
+                nombre:this.nombre,
+                precio:this.precio,
+                cantidad:this.cantidad
+            }
+            this.productos.push(producto);
+        }
+    
+    }
+}).mount('#app')
